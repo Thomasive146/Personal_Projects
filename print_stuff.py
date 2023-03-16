63b20ff7617c279995a06a89543acf78dd8e5067
Thomasive146 <128088411+Thomasive146@users.noreply.github.com>
2023-03-16 19:56:17 +0100

    Add files via upload

---
+++ print_stuff.py
@@ -0,0 +1,28 @@
+def print_field(x):
+    print()
+
+    print(x[0][0], end=" ")
+    print(x[0][1], end=" ")
+    print(x[0][2], end=" ")
+    print(x[0][3], end=" ")
+    print()
+
+    print(x[1][0], end=" ")
+    print(x[1][1], end=" ")
+    print(x[1][2], end=" ")
+    print(x[1][3], end=" ")
+    print()
+
+    print(x[2][0], end=" ")
+    print(x[2][1], end=" ")
+    print(x[2][2], end=" ")
+    print(x[2][3], end=" ")
+    print()
+
+    print(x[3][0], end=" ")
+    print(x[3][1], end=" ")
+    print(x[3][2], end=" ")
+    print(x[3][3], end=" ")
+    print()
+
+    print()
